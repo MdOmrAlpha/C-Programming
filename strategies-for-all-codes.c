08ffaf26eafb2ec3c09ca7e2d32772f845f02b6f
Md. Omar Yusuf(ET251082) <omaryusufultrainstinct@gmail.com>
2025-03-21 22:38:54 +0600

    Create strategies-for-all-codes.c

---
+++ strategies-for-all-codes.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+    printf("All strategies\n");
+    printf("1. Area of Rectangle = length*breadth\n");
+    printf("2. Area of Triangle(both acute and right anled) = 0.5*base*height\n");
+    printf("3. Area of Circle = 3.1416*r*r\n");
+    printf("4. Radius of Circle = sqrt(a/3.1416)\n");
+    printf("5. Celcius to Fahrenheit = ((c/5)*9)+32\n");
+    printf("6. Fahrenheit to Celcius = ((f-32)/9)*5\n");
+    printf("7. Inches to Centimeters = 2.54*I\n");
+    printf("8. Centimeters to Inches = 0.3937*C\n");
+    printf("9. Square Root of a Number = sqrt(a)\n");
+    printf("10. Square of a Number = a*a\n");
+    printf("11. Greatest of Three Numbers =  if (a >= b && a >= c):a is greatest; else if (b >= a && b >= c):b is greatest; else:c is greatest\n");
+    printf("12. Smallest of Three Numbers = if (a <= b && a <= c):a is smallest no;else if (b <= a && b <= c):b is smallest no;else:c is smallest no\n");
+    printf("13. Even or Odd = if a%2==0, else Odd\n");
+    printf("14. Prime or Not =  if (num <= 1): Not prime, else for (i = 2; i < num; i++) {if (num % i == 0): not prime} else: prime\n");
+    printf("15. Addition of Two Numbers = a+b\n");
+    printf("16. Subtraction of Two Numbers = a-b\n");
+    printf("17. Multiplication of Two Numbers = a*b\n");
+    printf("18. Division of Two Numbers = a/b\n");
+    printf("19. Modulus of Two Numbers = a%b\n");
+
+
+    return 0;
+}
