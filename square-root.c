6a86031cd38c7a7dfc4bc8bf3a01c8405eb1c5d2
Md. Omar Yusuf(ET251082) <omaryusufultrainstinct@gmail.com>
2025-03-21 22:03:51 +0600

    Create square-root.c

---
+++ square-root.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+
+    printf("Enter a number: ");
+    scanf("%f", &a);
+
+    b = sqrt(a);
+
+  printf("Square root of a number: %f", b);
+return 0;
+}
