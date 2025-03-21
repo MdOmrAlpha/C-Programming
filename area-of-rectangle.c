ae390e30487418d40a0e02779d2d11cad48f52b5
Md. Omar Yusuf(ET251082) <omaryusufultrainstinct@gmail.com>
2025-03-21 22:00:20 +0600

    Create area-of-rectangle.c

---
+++ area-of-rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int l, b, a;
+
+    printf("Enter length and breadth: ");
+    scanf("%d %d", &l, &b);
+
+    a = l * b;
+
+    printf("Area of rectangle:%d", a);
+return 0;
+}
