cc860d689b93fe5707ae844fd99994cc487eadd0
Md. Omar Yusuf(ET251082) <omaryusufultrainstinct@gmail.com>
2025-03-21 22:04:59 +0600

    Create square-of-a-number.c

---
+++ square-of-a-number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float a, b;
+
+    printf("Enter a number: ");
+    scanf("%f", &a);
+
+    b = a * a;
+
+  printf("Square value a number: %f", b);
+return 0;
+}
